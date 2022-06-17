e1dd304cf2466d89f44e2f46ae7a933ffe1016c5
Евгений <emv2298@icloud.com>
2022-06-18 01:09:24 +0300

    Правки

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function getRandomNumber (min, max) {
-  if (min >= 0 | max > min) {
+  if (min >= 0 && max > min) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   return 'Числа должны быть больше нуля и максимальное число должно быть больше';
@@ -8,9 +8,9 @@ function getRandomNumber (min, max) {
 getRandomNumber(5, 10);
 
 function getFloatingPointNumber (min, max, count) {
-  if (count > 20 | count < 0 ) {
+  if (count > 20 && count < 0 ) {
     return 'Колтчество знаков после запятой должно быть от 0 до 20';
-  } else if (min < 0 | max < min) {
+  } else if (min < 0 && max < min) {
     return 'Числа должны быть больше нуля и максимальное число должно быть больше';
   } else {
     return parseFloat((Math.random() * (max - min) + min).toFixed(count));
