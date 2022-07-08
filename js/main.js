f438436c779562f4da6d4da162c0e02af95eec1c
Евгений <emv2298@icloud.com>
2022-07-09 01:13:07 +0300

    task 7.2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 import {createElements} from './create-elements.js';
 import { generateCards } from './create-cards.js';
-
+import {changeFormStatus} from './form.js';
+const status = false;
+changeFormStatus(status);
 generateCards(createElements());
